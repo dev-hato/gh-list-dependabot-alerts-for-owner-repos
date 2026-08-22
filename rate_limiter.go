0f8804e1ef68e782f83b16cd1eebae15a2285e55
masaya.suzuki <suzukimasaya428@gmail.com>
2026-08-23 07:05:14 +0900

    Split newLimiter back out of the limiter var

--- rate_limiter.go
+++ rate_limiter.go
@@ -6,4 +6,8 @@ import (
 
 // limiter throttles outgoing GitHub API requests to a steady rate,
 // so that per-repository fetches issued in parallel don't burst against GitHub all at once.
-var limiter = rate.NewLimiter(5, 1)
+var limiter = newLimiter()
+
+func newLimiter() *rate.Limiter {
+	return rate.NewLimiter(5, 1)
+}
